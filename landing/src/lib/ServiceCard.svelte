c62464baf0bda8e597683e90ca8ab85bc0a8f8fc
fekoneko <55813967+fekoneko@users.noreply.github.com>
2025-02-27 01:45:24 +0300

    landing: page layout

---
+++ landing/src/lib/ServiceCard.svelte
@@ -0,0 +1,21 @@
+<script lang="ts">
+  import colors from 'tailwindcss/colors';
+
+  export let href: string;
+  export let name: string;
+  export let color: keyof typeof colors;
+</script>
+
+<a
+  {href}
+  class="focus:*:ring-3 focus:*:ring-offset-3 not-hover:not-focus:*:opacity-80 block h-20 min-w-36 hover:*:-translate-y-1 hover:*:shadow-xl focus:outline-none sm:h-28"
+>
+  <div
+    class="shadow-current/15 duration-400 flex size-full flex-col items-center justify-center rounded-3xl border-2 ring-current transition-all"
+    style:background-color={colors[color][50]}
+    style:border-color={colors[color][300]}
+    style:color={colors[color][500]}
+  >
+    <p class="text-center text-xl font-bold text-{color}-600">{name}</p>
+  </div>
+</a>
--- landing/src/routes/+page.svelte
+++ landing/src/routes/+page.svelte
@@ -1,2 +1,25 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://svelte.dev/docs/kit">svelte.dev/docs/kit</a> to read the documentation</p>
+<script lang="ts">
+  import { PUBLIC_MAIN_DOMAIN_NAME as MAIN_DOMAIN } from '$env/static/public';
+  import ServiceCard from '$lib/ServiceCard.svelte';
+</script>
+
+<div class="size-full bg-gradient-to-tl from-rose-300 to-orange-200 p-6">
+  <div
+    class="flex size-full flex-col items-center justify-center gap-12 rounded-3xl bg-white p-8 shadow-xl shadow-rose-900/5 md:p-12"
+  >
+    <div class="grow sm:h-16"></div>
+
+    <h1 class="text-center font-serif text-5xl font-bold text-pink-950 sm:text-6xl">
+      Welcome to
+      <span class="text-pink-600">{MAIN_DOMAIN}</span>
+    </h1>
+
+    <section
+      class="grid w-full max-w-[48rem] grow grid-cols-1 content-end gap-4 sm:grid-cols-3 sm:gap-8"
+    >
+      <ServiceCard href="https://searxng.{MAIN_DOMAIN}" name="SearXNG" color="indigo" />
+      <ServiceCard href="https://traefik.{MAIN_DOMAIN}" name="Traefik" color="emerald" />
+      <ServiceCard href="https://wireguard.{MAIN_DOMAIN}" name="WireGuard" color="fuchsia" />
+    </section>
+  </div>
+</div>
