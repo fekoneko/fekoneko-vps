1f895333f8c49c72d1f1e14716e81d716fd57d79
fekoneko <55813967+fekoneko@users.noreply.github.com>
2025-02-27 23:24:42 +0300

    landing: go next button

---
+++ landing/src/lib/icons/ArrowRightIcon.svelte
@@ -0,0 +1,16 @@
+<script lang="ts">
+  import type { SVGAttributes } from 'svelte/elements';
+
+  type $$Props = SVGAttributes<SVGSVGElement>;
+</script>
+
+<svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" {...$$props}>
+  <path
+    fill="none"
+    stroke="currentColor"
+    stroke-linecap="round"
+    stroke-linejoin="round"
+    stroke-width="1.5"
+    d="M4 12h16m0 0l-6-6m6 6l-6 6"
+  />
+</svg>
--- landing/src/routes/+page.svelte
+++ landing/src/routes/+page.svelte
@@ -1,15 +1,20 @@
 <script lang="ts">
   import { PUBLIC_MAIN_DOMAIN_NAME as MAIN_DOMAIN } from '$env/static/public';
-  import ServiceCard from '$lib/ServiceCard.svelte';
+  import GoNextButton from '$lib/components/GoNextButton.svelte';
+  import ServiceCard from '$lib/components/ServiceCard.svelte';
 </script>
 
 <div class="size-full bg-gradient-to-tl from-rose-300 to-orange-200 p-6">
   <div
-    class="flex size-full flex-col items-center justify-center gap-12 rounded-3xl bg-white p-8 shadow-xl shadow-rose-900/5 md:p-12"
+    class="scroll-hidden flex size-full flex-col items-center justify-center gap-12 overflow-x-hidden overflow-y-scroll rounded-3xl bg-white p-8 shadow-xl shadow-rose-900/5 md:p-12"
   >
-    <div class="grow sm:h-16"></div>
+    <div class="flex h-0 w-full grow items-start justify-end sm:h-16">
+      <GoNextButton />
+    </div>
 
-    <h1 class="xs:text-5xl text-center font-serif text-4xl font-bold text-pink-950 sm:text-6xl">
+    <h1
+      class="xs:text-5xl text-center font-serif text-4xl font-bold text-pink-950 transition-all duration-300 sm:text-6xl"
+    >
       Welcome to
       <span class="text-pink-600">{MAIN_DOMAIN}</span>
     </h1>
