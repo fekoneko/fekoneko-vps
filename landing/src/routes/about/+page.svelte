15831d09809bc8cedb9d571000ef7e4d3005e237
fekoneko <55813967+fekoneko@users.noreply.github.com>
2025-03-01 00:11:26 +0300

    landing: about page content

--- landing/src/routes/about/+page.svelte
+++ landing/src/routes/about/+page.svelte
@@ -11,14 +11,29 @@
   <ArrowLink href="/" label="Back" direction="left" />
 </div>
 
-<figure class="flex grow flex-col items-center gap-6">
-  <img
-    src="/fekoneko-avatar.jpg"
-    alt="fekoneko avatar"
-    class="xs:size-48 size-40 rounded-full border-4 border-dashed border-neutral-200"
-  />
-  <figcaption class="xs:text-3xl text-center text-2xl font-bold text-pink-600">fekoneko</figcaption>
-</figure>
+<div class="flex grow flex-col gap-11">
+  <figure class="flex flex-col items-center gap-6">
+    <img
+      src="/fekoneko-avatar.jpg"
+      alt="fekoneko avatar"
+      class="xs:size-48 size-40 rounded-full border-4 border-dashed border-neutral-200 transition-all"
+    />
+    <figcaption class="xs:text-3xl text-center text-2xl font-bold text-pink-600">
+      fekoneko
+    </figcaption>
+  </figure>
+
+  <div class="flex max-w-[24rem] flex-col gap-5 font-serif tracking-wider sm:max-w-none sm:gap-4">
+    <p class="xs:text-2xl **:font-normal text-center text-[1.35rem] text-neutral-500">
+      <span class="whitespace-nowrap"> I love open source, open </span>{' '}
+      <span class="whitespace-nowrap"> art and open people </span>
+    </p>
+    <p class="text-center text-lg text-neutral-400">
+      <span class="whitespace-nowrap">Also I've heard that hosting</span>{' '}
+      <span class="whitespace-nowrap">everything yourself is cool</span>
+    </p>
+  </div>
+</div>
 
 <a
   href="https://github.com/fekoneko"
