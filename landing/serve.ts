7622e98f52844b55451865be2d236cbc62f050a8
fekoneko <55813967+fekoneko@users.noreply.github.com>
2026-03-05 22:34:59 +0300

    landing: bun serve

--- landing/serve.ts
+++ landing/serve.ts
@@ -1,6 +1,18 @@
+import { readdir } from "node:fs/promises";
+
+const fileRoutes: Bun.Serve.Options<undefined, string>["routes"] = {};
+
+for (const path of await readdir("./dist", { recursive: true })) {
+  fileRoutes["/" + path] = Bun.file(`./dist/${path}`);
+}
+
 const server = Bun.serve({
   port: 3000,
-  routes: {},
+  routes: {
+    ...fileRoutes,
+    "/": Bun.file("./dist/index.html"),
+    "/*": Response.redirect("/"),
+  },
 });
 
 console.log(`Server running at ${server.url}`);
